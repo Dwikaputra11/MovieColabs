235732f28ca3209d40e0cb3a4eac45043c959e0f
Dwika <dwikariswanda@gmail.com>
2022-10-03 22:22:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/moviecolabs/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.moviecolabs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    /**
+     * TODO: LOGIN + splashscreen =
+        BUAT ADAPTER + Reycycler + ViewModel =
+        Get API (get, create, update, delete =
+     * */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
